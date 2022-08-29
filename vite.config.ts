95e304bfe52c10122837f976ee268328ff347be6
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2022-08-30 01:19:18 +0300

    Used terser minifier

--- vite.config.ts
+++ vite.config.ts
@@ -35,6 +35,7 @@ export default defineConfig((environment) => {
 			plugins: [...sharedOptions.plugins!],
 			build: {
 				cssCodeSplit: false,
+				minify: 'terser',
 			},
 			css: {
 				postcss: {
